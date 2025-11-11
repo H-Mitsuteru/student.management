d0ac2cc999757b22455b94658c61f980230d64eb
Yashima <terubo3-_-3pikka@docomo.ne.jp>
2025-11-11 22:02:47 +0900

    スタンダート第38回 リファクタリング

--- src/main/java/raisetech/student/management/Controller/StudentController.java
+++ src/main/java/raisetech/student/management/Controller/StudentController.java
@@ -8,6 +8,7 @@ import javax.lang.model.element.ModuleElement;
 import javax.naming.Binding;
 import org.apache.ibatis.annotations.Insert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.Controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
@@ -22,32 +24,40 @@ import raisetech.student.management.data.StudentsCourses;
 import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.service.StudentService;
 
-@Controller
+/**
+ * 受講生の検索や登録、更新などを行うREST APIとして実行されるControllerです。
+ */
+@RestController
 public class StudentController {
 
   private StudentService service;
-  private StudentConverter converter;
 
   @Autowired
-  private StudentController(StudentService service, StudentConverter converter) {
+  private StudentController(StudentService service) {
     this.service = service;
-    this.converter= converter;
   }
 
+  /**
+   * 受講生一覧検索です。
+   *　全件検索を行うので、条件指定はしません。
+   *
+   * @return　受講生一覧(全件)
+   */
   @GetMapping("/studentList")
-  public String getStudentList(Model model) { // 個別情報取得
-    List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
-
-    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
-    return "studentList";
+  public List<StudentDetail> getStudentList() { // 個別情報取得
+    return  service.searchStudentList();
   }
 
+  /**
+   * 受講生検索です。
+   * IDに紐づく任意の受講生の情報を取得します。
+   *
+   * @param studentID　受講生ID
+   * @return　受講生
+   */
   @GetMapping("/student/{studentID}")
-  public String getStudent(@PathVariable String studentID, Model model) {
-    StudentDetail studentDetail = service.searchStudent(studentID);
-    model.addAttribute("studentDetail", studentDetail);
-    return "updateStudent";
+  public StudentDetail getStudent(@PathVariable String studentID) {
+    return service.searchStudent(studentID);
   }
 
   @GetMapping("/newStudent")
@@ -59,26 +69,18 @@ public class StudentController {
   }
 
   @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if(result.hasErrors()) {  // エラーがあったら元の画面に戻す
-      return "resisterStudent";
-    }
-    // 新規受講生情報を登録する処理を実装する
-    service.registerStudent(studentDetail);
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
+    StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
     // コース情報も一緒に登録できる様に実装する。コースは単体で良い。
-  return "redirect:/studentList"; // 受講一覧を実行
+  return ResponseEntity.ok(responseStudentDetail);
   }
 
 
   @PostMapping("/updateStudent")
-  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if(result.hasErrors()) {  // エラーがあったら元の画面に戻す
-      return "updateStudent";
-    }
+  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
-    return "redirect:/studentList"; // 受講一覧を実行
+    return ResponseEntity.ok("更新処理が成功しました。");
   }
-
 }
 
 //  @GetMapping("/studentsCourseList")
